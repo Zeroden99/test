6c8c5c168f79b86990ffd6f1f0df81bd4120b9d2
Zeroden99 <44289240+Zeroden99@users.noreply.github.com>
2023-09-04 12:09:27 +0300

    add delete and update comment

--- controllers/commentControllers.js
+++ controllers/commentControllers.js
@@ -19,8 +19,14 @@ class commentControllers {
     async deleteComment(req, res, next) {
         try {
             const comment = await Comment.findById(req.params.id)
-            const post = await Post.findById(req.params.id)
-            if(req.user.id === comment.userId || req.user.id === post.userId){
+            if (!comment) {
+                return next(createError(404, 'Comment not found'));
+            }
+            const post = await Post.findById(comment.postId)
+            if (!post) {
+                return next(createError(404, 'Post not found'));
+            }
+            if (comment.userId.toString() == req.user.id || post.userId.toString() == req.user.id){
                 await Comment.findByIdAndDelete(req.params.id)
                 res.status(200).json('Comment has been deleted')
             }
@@ -30,6 +36,30 @@ class commentControllers {
         } catch (e) {
             next(e)
         }
+    }
+    async updateComment(req, res, next) {
+        try {
+            const comment = await Comment.findById(req.params.id)
+            if (!comment) {
+                return next(createError(404, 'Comment not found'));
+            }
+            const post = await Post.findById(comment.postId)
+            if (!post) {
+                return next(createError(404, 'Post not found'));
+            }
+            if (comment.userId.toString() == req.user.id || post.userId.toString() == req.user.id) {
+                const updateComm = await Comment.findByIdAndUpdate(req.params.id, {
+                    $set: req.body
+                }, 
+                {new: true}
+                )
+                res.status(200).json(updateComm)
+            } else {
+                return next(createError(403, 'You can update only your comment'))
+            }
+        } catch (e) {
+            next(e)
+        } 
     }
     async getComment(req, res, next) {
         try {
